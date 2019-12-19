6a92b3cf2befd621ff307bf8273c7af24941cbe7
PizzamanJ10 <55109111+PizzamanJ10@users.noreply.github.com>
2019-12-19 21:28:30 +0200

    Chait update

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,34 @@
-public class Board {
-    public static void main(String[] args) {
-        System.out.println("hi");
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Board extends JFrame {
+    public Board() throws IOException {
+
+            BufferedImage background;
+            String backgroundSrc = "Background.png";
+
+            //Loading the image into Background
+
+            background = ImageIO.read(Board.class.getResource(backgroundSrc));
+
+
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(1080, 720);
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+
+            frame.setContentPane(new JPanel() {
+                @Override
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(background, 0, 0, null);
+                }
+            });
+            frame.setVisible(true);
+        }
     }
-}
+
